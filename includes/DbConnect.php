1c11b43928c06d3947456e82b0b63f199bcfba2c
Ajay Prabhakar <48018942+Chromicle@users.noreply.github.com>
2019-08-02 16:25:51 +0530

    Update DbConnect.php

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -3,7 +3,9 @@
         private $con; 
         function connect(){
             include_once dirname(__FILE__)  . '/Constants.php';
-            $this->con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
+            $dsn = "pgsql:host=" . DB_HOST . ";port=" . "5432" .";dbname=" . DB_NAME. ";user=" . DB_USER . ";password=" . DB_PASSWORD. ";";
+
+            $this->con = new PDO($dsn, DB_USER, DB_PASSWORD);
             if(mysqli_connect_errno()){
                 echo "Failed  to connect " . mysqli_connect_error(); 
                 return null; 

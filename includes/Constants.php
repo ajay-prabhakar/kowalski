a918098232d0678b2ed1603277083f77ac1eff83
Ajay Prabhakar <48018942+Chromicle@users.noreply.github.com>
2019-08-02 15:59:40 +0530

    Update Constants.php

--- includes/Constants.php
+++ includes/Constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_NAME','my_app');
+define('DB_HOST','ec2-174-129-194-188.compute-1.amazonaws.com');
+define('DB_USER','jbutwgepikedcz');
+define('DB_PASSWORD','1545e46f83fced7614d86830b06637a9fb7ed2493f976aa123be18a9a96956dc');
+define('DB_NAME','dkn21hnpos0u1');
 
 define('USER_CREATED',101);
 define('USER_EXISTS',102);

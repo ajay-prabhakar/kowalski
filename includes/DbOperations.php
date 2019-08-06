25d091f448bc4e24aae3baae993363f6102e0427
Ajay Prabhakar <48018942+Chromicle@users.noreply.github.com>
2019-08-06 22:27:26 +0530

    Update DbOperations.php

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -95,15 +95,7 @@
             $stmt = $this->con->prepare("SELECT id, email, name, school FROM users;");
             // $stmt->fetchColumn($id, $email, $name, $school);
             $stmt->execute(); 
-            $users = array(); 
-            while($stmt->fetch()){ 
-                $user = array(); 
-                $user['id'] = $id; 
-                $user['email']=$email; 
-                $user['name'] = $name; 
-                $user['school'] = $school; 
-                array_push($users, $user);
-            }             
+            $users = $stmt->fetch(PDO::FETCH_BOTH);             
             return $users; 
         }
 
